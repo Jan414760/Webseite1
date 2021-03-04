98df336586dbc3b6098bec8f3b661fa0e2129ce9
Jan414760 <janpatrick.jps@gmail.com>
2021-03-04 18:49:01 +0100

    Version0.1(pre-release1)

---
+++ script.js
@@ -0,0 +1,6 @@
+let canvas = document.createElement("canvas");
+canvas.style.backgroundColor = "black";
+canvas.style.height = 1024 + "px";
+canvas.style.width = 1024 + "px";
+
+document.body.appendChild(canvas);
